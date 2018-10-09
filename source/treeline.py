7560fa348e6742e803032b83530462dbc3623ac4
Doug Bell <doug101@bellz.org>
2018-10-08 21:24:17 -0400

    updated the version number

--- source/treeline.py
+++ source/treeline.py
@@ -13,7 +13,7 @@
 #******************************************************************************
 
 __progname__ = 'TreeLine'
-__version__ = '3.0.1'
+__version__ = '3.0.1+'
 __author__ = 'Doug Bell'
 
 docPath = None         # modified by install script if required
