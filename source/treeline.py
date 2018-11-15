5048688a487deca7df1b23fb17819aaffc5d7f7e
Doug Bell <doug101@bellz.org>
2018-11-14 21:42:02 -0500

    update documentation change log and web docs

--- source/treeline.py
+++ source/treeline.py
@@ -13,7 +13,7 @@
 #******************************************************************************
 
 __progname__ = 'TreeLine'
-__version__ = '3.0.2+'
+__version__ = '3.0.3'
 __author__ = 'Doug Bell'
 
 docPath = None         # modified by install script if required
